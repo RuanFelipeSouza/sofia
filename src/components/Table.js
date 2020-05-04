c7d323c9240d519e50524ab562acf92ccfb34a4b
Breno <breno@pop-os.localdomain>
2020-05-04 16:03:22 -0300

    corrige cor dos indicadores

--- src/components/Table.js
+++ src/components/Table.js
@@ -9,7 +9,7 @@ export default function Table(props) {
   const columns = [
     { title: 'ID', field: '_id', editable: 'never' },
     { title: 'Data', field: 'createdAt', editable: 'never' },
-    { title: 'Indicador', field: 'anythingElse', editable: 'never', render: props2 => props2.anythingElse ? <EmojiObjects style={{ color: green[500] }}/> : <EmojiObjects style={{ color: red[500] }}/>},
+    { title: 'Indicador', field: 'anythingElse', editable: 'never', render: props2 => props2.anythingElse ? <EmojiObjects style={{ color: red[500] }}/> : <EmojiObjects style={{ color: green[500] }}/>},
     { title: 'Visualizado', field: 'viewed', type: 'boolean', editable: 'onUpdate' },
     { 
       title: 'Detalhes', 
