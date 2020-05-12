7a45cd6b53cf103664a65f4baff877f1412d7db5
Renan Nascimento <Renan@costanascimento.com.br>
2020-05-12 09:29:57 -0300

    added hook to save state to local storage

---
+++ src/utils/useLocalStorageState.js
@@ -0,0 +1,38 @@
+const useLocalStorageState = (key, initialValue, useState) => {
+  const [storedValue, setStoredValue] = useState(() => {
+    try {
+      const item = window.localStorage.getItem(key);
+      return item ? JSON.parse(item) : initialValue;
+    } catch (error) {
+      console.log('Error getting value from loca storage', error);
+      return initialValue;
+    }
+  });
+
+  const setValue = value => {
+    try {
+      const valueToStore = value instanceof Function ? value(storedValue) : value;
+      setStoredValue(valueToStore);
+      window.localStorage.setItem(key, JSON.stringify(valueToStore));
+    } catch (error) {
+      console.log('Error setting value to local storage', error);
+    }
+  };
+
+  return [storedValue, setValue];
+};
+
+const keys = {
+  INTELLILOGS_PROJETO: 'INTELLILOGS_PROJETO',
+  INTELLILOGS_DATA_INICIO: 'INTELLILOGS_DATA_INICIO',
+  INTELLILOGS_DATA_FIM: 'INTELLILOGS_DATA_FIM'
+};
+
+const localStorageStateHook = {
+  useLocalStorageState,
+  keys
+};
+
+Object.freeze(localStorageStateHook);
+
+export default localStorageStateHook;
