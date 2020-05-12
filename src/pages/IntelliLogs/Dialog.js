9922652125979d6297d62644d665aff87fe13a00
Breno Zupeli <breno.zupeli@intelliway.com.br>
2020-05-12 12:08:55 -0300

    adiciona opção para exibir mensagem com imagem

--- src/pages/IntelliLogs/Dialog.js
+++ src/pages/IntelliLogs/Dialog.js
@@ -67,6 +67,9 @@ const useStyles = makeStyles((theme) => ({
     color: '#888',
     margin: '0',
     textAlign: 'right'
+  },
+  innerImage: {
+    maxHeight: '500px',
   }
 }));
 
@@ -79,40 +82,44 @@ function formatMessage(msg) {
   return '';
 }
 
+const createMessage = props => 
+  <>
+    <p>
+      <b>{row.from}</b><br/>
+      {row.media && (<> <img className={classes.innerImage} src={props.media} alt={""} /> <br/> </>)}
+      {formatMessage(props.text)}
+    </p>
+    <h5 className={classes.messageDatetime}>{moment(props.date).format("DD/MM/YYYY HH:mm")}</h5>
+  </>
+
 export default function Dialog(props) {
   const classes = useStyles();
   const { conversa } = props;
 
   return (
-      <Paper className={classes.conversation}>
-          {conversa.history && conversa.history.map((row) => {
-            if(row.from === 'Assistente') 
-                return <div key={row._id} className={classes.assistantLine}>
-                <Avatar className={classes.purpleAvatar}>A</Avatar>
-                <Paper className={classes.assistantMessage}>
-                    <p><b>{row.from}</b><br/>
-                    {formatMessage(row.text)}</p>
-                    <h5 className={classes.messageDatetime}>{moment(row.date).format("DD/MM/YYYY HH:mm")}</h5>
-                </Paper>
-                </div>
-            if(row.from === conversa.teacherName) 
-                return <div key={row._id} className={classes.teacherLine}>
-                <Avatar className={classes.greenAvatar}>{row.from.charAt()}</Avatar>
-                <Paper className={classes.teacherMessage}>
-                    <p><b>{row.from}</b><br/>
-                    {formatMessage(row.text)}</p>
-                    <h5 className={classes.messageDatetime}>{moment(row.date).format("DD/MM/YYYY HH:mm")}</h5>
-                </Paper>
-                </div>
-            return <div key={row._id} className={classes.studentLine}>
-                <Avatar className={classes.orangeAvatar}>{row.from.charAt()}</Avatar>
-                <Paper className={classes.studentMessage}>
-                <p><b>{row.from}</b><br/>
-                {formatMessage(row.text)}</p>
-                <h5 className={classes.messageDatetime}>{moment(row.date).format("DD/MM/YYYY HH:mm")}</h5>
-                </Paper>
-            </div>
-          })} 
-      </Paper>
+    <Paper className={classes.conversation}>
+      {conversa.history && conversa.history?.map((row) => {
+        if(row.from === 'Assistente') 
+          return <div key={row._id} className={classes.assistantLine}>
+          <Avatar className={classes.purpleAvatar}>A</Avatar>
+          <Paper className={classes.assistantMessage}>
+            {createMessage(row)}
+          </Paper>
+          </div>
+        if(row.from === conversa.teacherName) 
+          return <div key={row._id} className={classes.teacherLine}>
+          <Avatar className={classes.greenAvatar}>{row.from?.charAt()}</Avatar>
+          <Paper className={classes.teacherMessage}>
+            {createMessage(row)}
+          </Paper>
+          </div>
+        return <div key={row._id} className={classes.studentLine}>
+          <Avatar className={classes.orangeAvatar}>{row.from?.charAt()}</Avatar>
+          <Paper className={classes.studentMessage}>
+            {createMessage(row)}
+          </Paper>
+        </div>
+      })} 
+    </Paper>
   )
 }
