401a341e453b423e3b6df2e9d020228fbaaa59f6
Renan Nascimento <Renan@costanascimento.com.br>
2020-06-04 14:59:01 -0300

    added assign responsability feature

--- src/pages/Curadoria/Responsible.js
+++ src/pages/Curadoria/Responsible.js
@@ -8,14 +8,14 @@ import Paper from '@material-ui/core/Paper';
 import DatePicker from '../../components/Datepicker';
 import TextField from '@material-ui/core/TextField';
 import Select from '../../components/Select';
+import CircularProgress from '@material-ui/core/CircularProgress';
 import Button from '@material-ui/core/Button';
-import Logo from './../../assets/logointellilogs.png';
 import Copyright from '../../components/Copyright';
 import Sidebar from '../../components/Sidebar';
-import Table from './Table';
+import Table from './../../components/GenericTable';
 
 import api from '../../services/api';
-import localStorageStateHook from '../../utils/useLocalStorageState';
+import localStorageStateHook from './../../utils/useLocalStorageState';
 
 const useStyles = makeStyles((theme) => ({
   root: {
@@ -69,33 +69,56 @@ const useStyles = makeStyles((theme) => ({
   }
 }));
 
+const columns = [
+  { title: 'Arquivo', field: 'arquivo' },
+  { title: 'Tema', field: 'tema' },
+  { title: 'Responsável', field: 'responsavel' }
+];
+
+const options = {
+  selection: true
+};
+
 export default function Responsible() {
   const { keys, useLocalStorageState } = localStorageStateHook;
   const classes = useStyles();
-  const [dataInicio, setDataInicio] = useLocalStorageState(keys.INTELLILOGS_DATA_INICIO, new Date(), useState);
-  const [dataFim, setDataFim] = useLocalStorageState(keys.INTELLILOGS_DATA_FIM, new Date(), useState);
+  const [startDate, setStartDate] = useLocalStorageState(keys.INTELLILOGS_DATA_INICIO, new Date(), useState);
+  const [endDate, setEndDate] = useLocalStorageState(keys.INTELLILOGS_DATA_FIM, new Date(), useState);
   const [project, setProject] = useLocalStorageState(keys.INTELLILOGS_PROJETO, 'Login', useState);
-  const [page, setPage] = useLocalStorageState(keys.INTELLILOGS_PAGINA_ATUAL, 0, useState);
-  const [pageSize, setPageSize] = useLocalStorageState(keys.INTELLILOGS_TAMANHO_PAGINA, 5, useState);
   const [isLoading, setLoading] = useState(false);
-  const [arquivo, setArquivo] = useState('');
-  const [tema, setTema] = useState('');
-  const [responsaveis, setResponsaveis] = useState([]);
-  const [responsavel, setResponsavel] = useState('');
+  const [nButtonPressed, setNButtonPressed] = useState(0);
+  const [responsibles, setResponsibles] = useState([]);
+  const [selectedRows, setSelectedRows] = useState([]);
+  const [responsability, setResponsability] = useState('');
 
   useEffect(_ => {
     setLoading(true);
+    setResponsability('');
+    setSelectedRows([]);
     api.get('/responsaveis', {
       params: {
-        dataInicio,
-        dataFim,
+        dataInicio: startDate,
+        dataFim: endDate,
         projeto: project
       }
     }).then(response => {
+      setResponsibles(response.data);
       setLoading(false);
-      setResponsaveis(response.data);
     })
-  }, [dataInicio, dataFim, project]);
+  }, [startDate, endDate, project, nButtonPressed]);
+
+  const assignResponsability = async (e) => {
+    setLoading(true);
+    e.preventDefault();
+    const curationList = selectedRows.map(({ _id }) => _id);
+
+    try {
+      await api.put('/responsaveis', { curationList, responsability });
+      setNButtonPressed(nButtonPressed + 1);
+    } catch (e) {
+      setLoading(false);
+    }
+  };
 
   return (
     <div className={classes.root}>
@@ -108,73 +131,49 @@ export default function Responsible() {
               <Grid container direction='column'>
                 <Grid item xs className={classes.filter}>
                   <Select value={project} handleSelectChange={setProject} />
-                  <DatePicker value={dataInicio} handleChangeDate={setDataInicio} id={"data_inicio"} label={"Data inicial"} />
-                  <DatePicker value={dataFim} handleChangeDate={setDataFim} id={"data_fim"} label={"Data final"} />
+                  <DatePicker value={startDate} handleChangeDate={setStartDate} id={"data_inicio"} label={"Data inicial"} />
+                  <DatePicker value={endDate} handleChangeDate={setEndDate} id={"data_fim"} label={"Data final"} />
                 </Grid>
-                {/* <Grid item xs className={classes.filter} spacing={3}>
-                  <TextField
-                    name="arquivo"
-                    label="Arquivo"
-                    id="arquivo"
-                    value={arquivo}
-                    onChange={(e) => setArquivo(e.target.value)}
-                  />
-                  <TextField
-                    name="tema"
-                    label="Tema"
-                    id="tema"
-                    value={tema}
-                    onChange={(e) => setTema(e.target.value)}
-                  />
-                  <TextField
-                    name="responsavel"
-                    label="Responsável"
-                    id="responsavel"
-                    value={responsavel}
-                    onChange={(e) => setResponsavel(e.target.value)}
-                  />
-                </Grid> */}
               </Grid>
             </Paper>
 
-            {responsaveis.length > 0 &&
+            {selectedRows.length > 0 &&
               <Paper className={classes.paper}>
                 <Grid item xs className={classes.filter}>
                   <TextField
                     name="responsavel"
                     label="Responsável"
                     id="responsavel"
-                    value={responsavel}
-                    onChange={(e) => setResponsavel(e.target.value)}
+                    value={responsability}
+                    onChange={(e) => setResponsability(e.target.value)}
                   />
                   <Button
                     type="submit"
                     variant="contained"
                     color="primary"
                     className={classes.submit}
-                    // disabled={loading}
-                    // onClick={handleLogin}
+                    disabled={isLoading || !responsability}
+                    onClick={assignResponsability}
                     style={{ height: 46 }}
                   >
-                    Atribuir Responsabilidade
-                </Button>
+                    {isLoading ? (<CircularProgress size={25} />) : 'Atribuir Responsabilidade'}
+                  </Button>
                 </Grid>
               </Paper>
             }
 
-            {/* <Grid item xs={12}>
+            <Grid item xs={12}>
               <Paper className={classes.table}>
                 <Table
-                  atendimentos={atendimentos}
-                  setAtendimentos={setAtendimentos}
+                  title='Responsáveis'
+                  columns={columns}
+                  data={responsibles}
+                  options={options}
                   isLoading={isLoading}
-                  initialPage={page}
-                  onChangePage={setPage}
-                  pageSize={pageSize}
-                  onChangeRowsPerPage={setPageSize}
+                  onSelectionChange={setSelectedRows}
                 />
               </Paper>
-            </Grid> */}
+            </Grid>
           </Grid>
           <Box pt={4}>
             <Copyright />
