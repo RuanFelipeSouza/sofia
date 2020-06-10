af84c5b33ad1137a0a3e96489946b1868dfb84f3
Renan Nascimento <Renan@costanascimento.com.br>
2020-06-10 11:20:01 -0300

    added colors props to bar chart

--- src/components/GenericBarChart.js
+++ src/components/GenericBarChart.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import { BarChart, Bar, XAxis, YAxis, CartesianGrid, Tooltip, Legend } from 'recharts';
 
-const colors = ['#8884d8', '#82ca9d'];
+const defaultColors = ['#8884d8', '#82ca9d'];
 
-export default function GenericBarChart({ data, isStacked, interval }) {
+export default function GenericBarChart({ data, isStacked, interval, colors }) {
   return (
     <BarChart
-      width={1100}
+      width={700}
       height={350}
       data={data}
       margin={{ top: 15, right: 20, left: 20, bottom: 5, }}
@@ -18,7 +18,7 @@ export default function GenericBarChart({ data, isStacked, interval }) {
       <Legend layout="horizontal" verticalAlign="top" align="center" />
       {
         data.length > 0 && Object.keys(data[0]).map((key, index) => {
-          return key !== 'data' ? <Bar key={key} dataKey={key} stackId={isStacked ? 'a' : index} fill={colors[index - 1]} /> : null;
+          return key !== 'data' ? <Bar key={key} dataKey={key} stackId={isStacked ? 'a' : index} fill={colors ? colors[index - 1] : defaultColors[index - 1]} /> : null;
         })
       }
     </BarChart>
