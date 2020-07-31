fdd399368c8a3f1c0995ee3f8dd19aaee765c471
Renan Nascimento <Renan@costanascimento.com.br>
2020-07-31 09:03:10 -0300

    changed default server port to 3000

--- server.js
+++ server.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const path = require('path');
 
-const PORT = 9000;
+const PORT = 3000;
 const app = express();
 
 app.use(express.static(path.join(__dirname, 'build')));
