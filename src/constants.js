547ef1f40543687ea532c2907e456eca04052829
Renan Nascimento <Renan@costanascimento.com.br>
2020-08-07 07:38:27 -0300

    added theme colors on constants file

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,4 @@
 //Colors
 export const BASE_COLOR = '#4a148c';
+export const PRIMARY_COLOR = '#4A148C';
+export const SECONDARY_COLOR = '#AC222A';
--- src/theme.js
+++ src/theme.js
@@ -1,15 +1,16 @@
 import { createMuiTheme } from '@material-ui/core/styles';
+import { PRIMARY_COLOR, SECONDARY_COLOR } from './constants';
 
 const palette = {
   primary: {
     // light: will be calculated from palette.primary.main,
-    main: '#4A148C',
+    main: PRIMARY_COLOR,
     // dark: will be calculated from palette.primary.main,
     // contrastText: will be calculated to contrast with palette.primary.main
   },
   secondary: {
     // light: will be calculated from palette.secondary.main,
-    main: '#AC222A',
+    main: SECONDARY_COLOR,
     // dark: will be calculated from palette.secondary.main,
     // contrastText: will be calculated to contrast with palette.secondary.main
   },
